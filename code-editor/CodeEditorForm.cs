fa1a41019dcc35f5aaafb36dd322483ae06ab1d8
lucalewin <luca.lewin23@gmail.com>
2021-07-11 13:53:22 +0200

    implement create folder + delete folder functionalities

--- code-editor/CodeEditorForm.cs
+++ code-editor/CodeEditorForm.cs
@@ -13,11 +13,11 @@ using System.Windows.Forms;
 
 namespace Lucraft.CodeEditor
 {
-    public partial class CodeEditorForm : Form
+    internal partial class CodeEditorForm : Form
     {
         private readonly List<string> openFiles = new();
         private readonly List<EditorControl> openEditors = new();
-        private EditorControl CurrentEditor = null;
+        // private EditorControl CurrentEditor = null;
 
         public CodeEditorForm()
         {
@@ -25,11 +25,33 @@ namespace Lucraft.CodeEditor
             treeView.NodeMouseDoubleClick += TreeView_NodeMouseDoubleClick;
             treeView.NodeMouseClick += TreeView_NodeMouseClick;
             treeView.AfterLabelEdit += TreeView_AfterLabelEdit;
+            treeView.MouseDown += TreeView_MouseDown;
+        }
+
+        private void TreeView_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                contextMenuStrip1.Show(treeView, e.Location);
+            }
         }
 
         private void TreeView_AfterLabelEdit(object sender, NodeLabelEditEventArgs e)
         {
-            // TODO: rename file in filesystem
+            // rename file in filesystem
+            var parentDirectoryInfo = e.Node.Parent.Tag as DirectoryInfo;
+            var directoryInfo = new DirectoryInfo(Path.Combine(parentDirectoryInfo.FullName, e.Node.Text));
+            if (!directoryInfo.Exists)
+            {
+                // create folder
+                directoryInfo.Create();
+            }
+            else
+            {
+                // rename folder
+                Directory.Move((e.Node.Tag as DirectoryInfo).FullName, directoryInfo.FullName);
+            }
+            e.Node.Tag = directoryInfo;
         }
 
         private void TreeView_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
@@ -103,18 +125,18 @@ namespace Lucraft.CodeEditor
         /// <param name="path"></param>
         public void OpenFile(string path)
         {
-            Debug.WriteLine(path);
+            //Debug.WriteLine(path);
             if (openFiles.Contains(path))
                 return;
             openFiles.Add(path);
             
 
             EditorControl editor = CodeEditor.GetEditorForFile(new FileInfo(path));
-            editor.Code = File.ReadAllText(path);
+            //editor.Code = File.ReadAllText(path);
             editor.Dock = DockStyle.Fill;
 
             openEditors.Add(editor);
-            CurrentEditor = editor;
+            //CurrentEditor = editor;
 
             var tabPage = new TabPage
             {
@@ -148,20 +170,59 @@ namespace Lucraft.CodeEditor
         }
         #endregion
 
-        private void SaveAllToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            openEditors.ForEach(o => o.Save());
-        }
+        private void SaveAllToolStripMenuItem_Click(object sender, EventArgs e) => openEditors.ForEach(o => o.InvokeSave());
 
         private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
         {
             //CurrentEditor.Save();
-            openEditors[tabControl1.SelectedIndex].Save();
+            openEditors[tabControl1.SelectedIndex].InvokeSave();
         }
 
         private void RenameToolStripMenuItem_Click(object sender, EventArgs e)
         {
             treeView.SelectedNode.BeginEdit();
+        }
+
+        private void TreeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            contextMenuStrip1.Hide();
+        }
+
+        private void DeleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string currentDir = CodeEditor.Current.Path;
+            if (treeView.SelectedNode.Tag == null)
+            {
+                // its a file
+                File.Delete(currentDir[..(currentDir.LastIndexOf("\\") + 1)] + treeView.SelectedNode.FullPath);
+                treeView.SelectedNode.Remove();
+            }
+            else if (treeView.SelectedNode.Parent == null) { } // this is the project root which cannot be delete from here
+            else
+            {
+                // its a folder
+                // ask for confirmation to delete folder + all its contents
+                if (MessageBox.Show("Do you want to delete this folder and all it's contents?", 
+                                    $"Delete '{treeView.SelectedNode.FullPath[(treeView.SelectedNode.FullPath.IndexOf("\\") + 1)..]}'", 
+                                    MessageBoxButtons.YesNoCancel, 
+                                    MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    Directory.Delete(currentDir[..(currentDir.LastIndexOf("\\") + 1)] + treeView.SelectedNode.FullPath, true);
+                    treeView.SelectedNode.Remove();
+                }
+            }
+        }
+
+        private void toolStripMenuItem10_Click(object sender, EventArgs e)
+        {
+            if (treeView.SelectedNode.Parent == null || treeView.SelectedNode.Tag != null)
+            {
+                // project root or folder
+                var newNode = new TreeNode("New Folder");
+                treeView.SelectedNode.Nodes.Add(newNode);
+                treeView.SelectedNode.Expand();
+                newNode.BeginEdit();
+            }
         }
     }
 }
